1a9b060b87db810615f5812bbdd612d4872c5b6b
Junaid Amin Bhat <junihbhatt@gmail.com>
2023-08-02 02:39:08 +0530

    worked on main page design

---
+++ assets/script/script.js
@@ -0,0 +1,10 @@
+$('.images-slider').slick({
+    slidesToShow: 1,
+    infinite: true,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 2000,
+    dots: true,
+    arrows: false,
+    pauseOnHover: false
+})
