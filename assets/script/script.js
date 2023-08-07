92e71791f808b9b3ec989faf49228622f7e9d0c8
Junaid Amin Bhat <junihbhatt@gmail.com>
2023-08-08 02:44:34 +0530

    made changes in inputs and navigations

--- assets/script/script.js
+++ assets/script/script.js
@@ -4,4 +4,28 @@ $(document).ready(function () {
                             <p>Trending</p>
                         </div>`);
     });
+    $(window).scroll(function () {
+        var scroll = $(window).scrollTop();
+        if (scroll >= 5) {
+            $("header").addClass("scrolled");
+        } else {
+            $("header").removeClass("scrolled");
+        }
+        var destinationsPosition = $('#destinatinations').offset().top;
+        var contactUsPosition = $('#contact-us').offset().top;
+        // console.log(contactUsPosition, scroll);
+        if (scroll <= $('.hero').height()) {
+            $('.main-nav .nav-item').removeClass('active');
+            $('.home-nav').addClass('active');
+        }
+        if (destinationsPosition <= scroll + 100) {
+            $('.main-nav .nav-item').removeClass('active');
+            $('.destinations-nav').addClass('active');
+        }
+        if (contactUsPosition <= scroll + 100) {
+            $('.main-nav .nav-item').removeClass('active');
+            $('.contact-us-nav').addClass('active');
+        }
+    });
+
 });
--- index.php
+++ index.php
@@ -25,28 +25,28 @@
                 </div>
                 <nav class="main-nav">
                     <ul class="main-nav__list">
-                        <li class="nav-item active">
+                        <li class="nav-item home-nav active">
                             <a href="#" class="nav-item__link">Home</a>
                         </li>
-                        <li class="nav-item">
+                        <!-- <li class="nav-item">
                             <a href="#" class="nav-item__link">Packages</a>
+                        </li> -->
+                        <li class="nav-item destinations-nav">
+                            <a href="#destinatinations" class="nav-item__link">Destinations</a>
                         </li>
-                        <li class="nav-item">
-                            <a href="#" class="nav-item__link">About</a>
+                        <li class="nav-item contact-us-nav">
+                            <a href="#contact-us" class="nav-item__link">Contact Us</a>
                         </li>
-                        <li class="nav-item">
-                            <a href="#" class="nav-item__link">Destinations</a>
-                        </li>
-                        <li class="nav-item">
+                        <!-- <li class="nav-item">
                             <a href="#" class="nav-item__link">Services</a>
-                        </li>
-                        <li class="nav-item">
+                        </li> -->
+                        <!-- <li class="nav-item">
                             <a href="#" class="nav-item__link">Gallery</a>
-                        </li>
+                        </li> -->
                     </ul>
                 </nav>
                 <div class="cta-box">
-                    <button class="btn btn--pri btn--cta">Book Now</button>
+                    <a href="#contact-us" class="btn btn--pri btn--cta">Book Now</a>
                 </div>
             </div>
         </div>
@@ -70,7 +70,8 @@
                                 embraces adventure! Book now for an extraordinary experience like no other.
                             </p>
                             <div class="main-section__cta">
-                                <button class="btn btn--pri btn--lg btn--cta">Book your kashmir adventure now!</button>
+                                <a href="#contact-us" class="btn btn--pri btn--lg btn--cta">Book your kashmir adventure
+                                    now!</a>
                             </div>
                         </div>
                         <div class="img-side">
@@ -82,6 +83,7 @@
         </section>
         <section class="showcase">
             <div class="container">
+                <div id="destinatinations" class="nav-empty-location"></div>
                 <div class="section-title">
                     <h2>
                         Our <span>Destinations</span>
@@ -181,7 +183,7 @@
                 </div>
             </div>
         </section>
-        <section class="contact-us">
+        <section class="contact-us" id="contact-us">
             <div class="container">
                 <div class="contact-content">
                     <div class="text-side">
@@ -200,24 +202,38 @@
                     <div class="form-side">
                         <form id="contact-form" class="form-contents" action="./email.php" method="POST">
                             <div class="contact-row row--2">
-                                <div class="input-group">
-                                    <input type="text" name="username" placeholder="Name">
+                                <div class="input-box">
+                                    <div class="input-group">
+                                        <input type="text" name="username" placeholder=" ">
+                                        <span class="floating-label">Name</span>
+                                    </div>
                                 </div>
-                                <div class="input-group">
-                                    <input type="text" name="phone" placeholder="Contact Number">
+                                <div class="input-box">
+                                    <div class="input-group">
+                                        <input type="text" name="phone" placeholder=" ">
+                                        <span class="floating-label">Contact Number</span>
+                                    </div>
                                 </div>
                             </div>
                             <div class="contact-row row--2">
-                                <div class="input-group">
-                                    <input type="text" name="email" placeholder="Email Id (optional)">
+                                <div class="input-box">
+                                    <div class="input-group">
+                                        <input type="text" name="email" placeholder=" ">
+                                        <span class="floating-label">Email (optional)</span>
+                                    </div>
                                 </div>
-                                <div class="input-group">
-                                    <input type="date" name="date" placeholder="Date of arrival">
+                                <div class="input-box">
+                                    <div class="input-group">
+                                        <input type="date" name="date" placeholder="">
+                                        <span class="floating-label">Date of arrival</span>
+                                    </div>
                                 </div>
                             </div>
                             <div class="contact-row">
-                                <div class="input-group">
-                                    <textarea name="desc" id="" placeholder="Message"></textarea>
+                                <div class="input-box">
+                                    <div class="input-group">
+                                        <textarea name="desc" id="" placeholder="Message"></textarea>
+                                    </div>
                                 </div>
                             </div>
                             <div class="form-action">
@@ -316,51 +332,60 @@
     </footer>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
     <link href="https://cdn.jsdelivr.net/npm/sweetalert2@11/dist/sweetalert2.min.css" rel="stylesheet">
-<script src="https://cdn.jsdelivr.net/npm/sweetalert2@11/dist/sweetalert2.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11/dist/sweetalert2.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery-validate/1.19.5/jquery.validate.min.js"
         integrity="sha512-rstIgDs0xPgmG6RX1Aba4KV5cWJbAMcvRCVmglpam9SoHZiUCyQVDdH2LPlxoHtrv17XWblE/V/PP+Tr04hbtA=="
         crossorigin="anonymous" referrerpolicy="no-referrer"></script>
     <script src="https://cdn.jsdelivr.net/jquery.slick/1.4.1/slick.min.js"></script>
     <script src="./assets/script/script.js"></script>
     <script type="text/javascript">
-        $(document).ready(function () {
-            $('#contact-form').validate({
-                rules: {
-                    username: 'required',
-                    phone: 'required',
-                    date: 'required',
-                },
-                messages: {
-                    username: 'Username is required',
-                    phone: 'Phone number is required',
-                    date: 'Date is required',
-                },
-                submitHandler: function (form) {
-                    $.ajax({
-                        url: './email.php', // Replace with your actual server URL
-                        method: 'POST', // HTTP method
-                        data: $(form).serialize(), // Serialize form data
-                        success: function (response) {
-                            // Handle success response from the server
-                            Swal.fire(
-                                'Succesfully Sent!',
-                                'You clicked the button!',
-                                'success'
-                            )
-                            $('#contact-form')[0].reset();
-                        },
-                        error: function (xhr, textStatus, errorThrown) {
-                            // Handle error response from the server
-                            Swal.fire({
-                                icon: 'error',
-                                title: 'Oops...',
-                                text: 'Something went wrong!',
-                            })
-                        }
-                    });
+    $(document).ready(function() {
+        $('#contact-form').validate({
+            rules: {
+                username: 'required',
+                phone: 'required',
+                date: 'required',
+                email: {
+                    email: true
                 }
-            });
+            },
+            messages: {
+                username: 'Name is required',
+                phone: 'Ph number is required',
+                date: 'Date is required',
+            },
+            submitHandler: function(form) {
+                $('.btn--frm-action').text('Sending...').addClass('disabled').attr("disabled",
+                    true);
+                $.ajax({
+                    url: './email.php', // Replace with your actual server URL
+                    method: 'POST', // HTTP method
+                    data: $(form).serialize(), // Serialize form data
+                    success: function(response) {
+                        // Handle success response from the server
+                        Swal.fire(
+                            'Succesfully Sent!',
+                            'Your message has been successfully sent',
+                            'success'
+                        );
+                        $('.btn--frm-action').text('Send').removeClass('disabled')
+                            .removeAttr("disabled");
+                        $('#contact-form')[0].reset();
+                    },
+                    error: function(xhr, textStatus, errorThrown) {
+                        // Handle error response from the server
+                        Swal.fire({
+                            icon: 'error',
+                            title: 'Oops...',
+                            text: 'Something went wrong!',
+                        });
+                        $('.btn--frm-action').text('Send').removeClass('disabled')
+                            .removeAttr("disabled");
+                    }
+                });
+            }
         });
+    });
     </script>
 </body>
 
